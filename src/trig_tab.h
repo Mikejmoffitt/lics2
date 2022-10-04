68df8e81042b28622bf0ae3b7db52ef824839a44
Michael Moffitt <mikejmoffitt@gmail.com>
2022-10-04 13:15:51 +0900

    trig_tab gets extern

--- src/trig_tab.h
+++ src/trig_tab.h
@@ -4,10 +4,10 @@
 #define TRIG_TAB_ATAN_INPUT_RANGE 64
 
 // Range 0 to 2 * pi
-const fix16_t trig_tab_sin[256];
-const fix16_t trig_tab_cos[256];
-const fix16_t trig_tab_tan[256];
+extern const fix16_t trig_tab_sin[256];
+extern const fix16_t trig_tab_cos[256];
+extern const fix16_t trig_tab_tan[256];
 // Range 0 to 64
-const fix16_t trig_tab_atan[4096];
+extern const fix16_t trig_tab_atan[4096];
 
 #endif  // TRIG_TAB_H
