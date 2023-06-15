d83f28a453bce6083c566122ddfd0a1fafcb9ca7
Michael Moffitt <mikejmoffitt@gmail.com>
2023-06-15 13:52:34 +0900

    Compression

--- compress_mapdata.sh
+++ compress_mapdata.sh
@@ -6,6 +6,9 @@ cd maps
 
 for f in *.bin
 do
-	../util/accurate-kosinski/kosinski-compress "$f" "../res/map/${f%.bin}.kos"
-	echo $f
+	dd if=$f of=hdr.bin bs=1 count=32
+	dd if=$f of=bulk.bin bs=1 skip=32
+	../util/accurate-kosinski/kosinski-compress bulk.bin bulk.kos
+	cat hdr.bin bulk.kos > "../res/map/${f%.bin}.map"
+	rm hdr.bin bulk.bin bulk.kos
 done
